5f32a3faa2f3026cef3fce8889d612ee7f5438bf
JeremieSordoillet <jeremie21.sordoillet@gmail.com>
2023-08-29 09:37:27 +0200

    Added a function.py where you can put your functions and I create a function to calculate the distance between home and away team

---
+++ functions.py
@@ -0,0 +1,24 @@
+## Functions ##
+
+# Libraries
+import math
+
+def haversine(lat1, lon1, lat2, lon2):
+    """
+    Calculate the distance between the home team and the away team
+    => return the distance in kilometer
+    """
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    radius = 6371.0
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = radius * c
+
+    return round(distance)
